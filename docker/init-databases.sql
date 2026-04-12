d2c1e9b2069b908390adfaa3f47ea2c505ab6acf
Gustavo Gomes <lgglima@gmail.com>
2026-04-12 09:36:47 -0300

    improve table ui.

--- docker/init-databases.sql
+++
@@ -1,4 +0,0 @@
--- Cria o banco de dados do FaturamentoService
--- O estoque_db já é criado automaticamente pelo POSTGRES_DB no docker-compose
-SELECT 'CREATE DATABASE faturamento_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'faturamento_db')\gexec
