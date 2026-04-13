1f7b96f9d129fd92dc2d168704b9a3d21ebe2b97
Gustavo Gomes <lgglima@gmail.com>
2026-04-13 10:43:38 -0300

    Update docker-compose to bind PostgreSQL to localhost and modify frontend service URLs to use localhost for production environment

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  estoqueServiceUrl: 'http://estoque-service',
-  faturamentoServiceUrl: 'http://faturamento-service',
+  estoqueServiceUrl: 'http://localhost:5189',
+  faturamentoServiceUrl: 'http://localhost:5002',
 };
